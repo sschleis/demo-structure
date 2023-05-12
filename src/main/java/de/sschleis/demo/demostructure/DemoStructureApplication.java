bb7fe4bc5b0e64b1e9f6229d3f3b1b764aeeb13a
sschleis <sebastian.schleis@gmail.com>
2023-05-12 23:58:44 +0200

    first commit

---
+++ src/main/java/de/sschleis/demo/demostructure/DemoStructureApplication.java
@@ -0,0 +1,13 @@
+package de.sschleis.demo.demostructure;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DemoStructureApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(DemoStructureApplication.class, args);
+    }
+
+}
---
+++ src/test/java/de/sschleis/demo/demostructure/DemoStructureApplicationTests.java
@@ -0,0 +1,13 @@
+package de.sschleis.demo.demostructure;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DemoStructureApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
